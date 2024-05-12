22d621bf7d898be9d039b98ed31c9f44b06b2b96
mdfaizu8941 <159682713+mdfaizu8941@users.noreply.github.com>
2024-05-12 10:41:07 +0530

    Add files via upload

---
+++ Q11 term.c
@@ -0,0 +1,55 @@
+ /* 
+ NAME                     : MOHAMMAD FAIZ
+ SECTION                : F
+ CLASS ROLL NO. : 44 
+Q11. Write a C program to read a text file named as “Data.txt”, Create an another file named as “Del.txt” deleting the words “a”, “the”, “an” from the file “Data.txt” and replacing each one of them with a blank space in “Del.txt”.
+*/
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    FILE *fp1,*fp2;
+char ch,word[500];
+int i ,j,count=0;
+fp1=fopen("Data.txt","w");
+if(fp1==NULL) printf("File Error");
+else
+{
+    while((ch=getchar())!=EOF)
+    {
+        fputc(ch,fp1);
+    }
+    fclose(fp1);
+}
+fp1=fopen("Data.txt","r");
+fp2=fopen("Del.txt","w");
+if(fp1==NULL&& fp2==NULL)
+printf("File Error");
+else
+{
+    while ((ch = fgetc(fp1)) != EOF)
+    {
+        if (ch == ' ' || ch == '\n'){
+         word[count] = '\0';
+            if (strcmp(word, "a") != 0 && strcmp(word, "the") != 0 && strcmp(word, "an") != 0) {
+                fprintf(fp2, "%s", word);
+            }
+            else {
+                fprintf(fp2, " ");
+            }
+          
+            fprintf(fp2, "%c", ch);
+            count = 0;
+        }
+        else {
+            word[count++] = ch;
+        }
+        
+    }
+    }
+    fclose(fp1);
+    fclose(fp2);
+
+    printf("\nFile processing complete.\n");
+    return 0;
+} 
