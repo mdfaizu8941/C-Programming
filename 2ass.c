a3f250243f078ffd5f97f8c6b64247de7fb9332d
mdfaizu8941 <159682713+mdfaizu8941@users.noreply.github.com>
2024-06-06 18:47:27 +0530

    longest from a file

--- 2ass.c
+++ 2ass.c
@@ -1,44 +1,38 @@
- /* Write an algorithm to input n numbers in a file and copy reverse of all even numbers in
-another file*/
+   
+       
 #include <stdio.h>
+#include<string.h>
 int main()
 {
-    FILE *fp,*f;
-    int n,i,res,num,c=0;
-    printf("inpu a numer limit: ");
-    scanf("%d",&n);
-    fp=fopen("Data.txt","w");
-    printf("input a numners: ");
-    for(i=0;i<n;i++)
+     FILE *f;
+     int i=0,n,max=0,min=100;
+     char str[100], mn[100],ma[100],c;
+     printf("Iput a paragraph:");
+     f=fopen("hello.txt","w");
+     while((c=getchar())!=EOF)
     {
-        scanf("%d",&num);
-        fprintf(fp,"%d ",num);
+       fputc(c,f); 
     }
-    fclose(fp);
-    fp=fopen("Data.txt","r");
-    f=fopen("even.tx","w");
-    for(i=0;i<n;i++)
+    fclose(f);
+    f=fopen("hello.txt","r");
+    while(fscanf(f,"%s",str)!=EOF)
+    { int l=strlen(str);
+        if(l >max)
     {
-        fscanf(fp,"%d",&num);
-        
-          res=0;
-        if(num%2==0)
-        {
-             while(num!=0)
-        {
-            int rev=num%10;
-            res=res*10+rev; 
-            num=num/10;
-            
-        }
-         fprintf(f,"%d ",res);
-        printf("%d ",res);
-        }
+        strcpy(ma,str);
+        max=l;
+    }
+    if(l<min)
+    {
+         strcpy(mn,str);
+        min=l;
+    }
         
     }
-    fclose(fp);
-        fclose(f);
+    printf("\nthe lonest word is %s and its length is %d\n",ma,max);
+     printf("the shorest word is %s and its length is %d",mn,min);
     
-
+    
+    fclose(f);
     return 0;
 }
